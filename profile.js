10f8d05c4d5396424d11abcf2317148e7a941ed2
xlnSystems <sasa@xlnsystems.com>
2022-03-19 12:36:52 -0400

    persist profile info to identity user_metadata

--- profile.js
+++ profile.js
@@ -1,14 +1,28 @@
 // Let's find these DOM elements!
-const nameInput = document.querySelector('#nameInput');
-const emailInput = document.querySelector('#emailInput');
+const nameDiv = document.querySelector('#nameInput');
+const emailDiv = document.querySelector('#emailInput');
+const streetDiv = document.querySelector('#streetDiv');
+const cityDiv = document.querySelector('#cityDiv');
+const stateDiv = document.querySelector('#stateDiv');
+const zipDiv = document.querySelector('#zipDiv');
 
 // format_stringify is a function that takes in an object and returns formatted JSON
 const format_stringify = (data) => JSON.stringify(data, null, 2);
 // showCurrentInfo updates the UI to show the current user data passed in to it
 const showCurrentInfo = (user) => {
   const str = format_stringify(user);
-  nameInput.innerHTML = user.user_metadata.full_name;
-  emailInput.innerHTML = user.email;
+  // console.log(user);
+  nameDiv.innerHTML = user.user_metadata.full_name;
+  emailDiv.innerHTML = user.email;
+  streetDiv.innerHTML = user.user_metadata.street_address;
+  document.querySelector('#streetInput').style.display = 'none';
+  cityDiv.innerHTML = user.user_metadata.city;
+  document.querySelector('#cityInput').style.display = 'none';
+  stateDiv.innerHTML = user.user_metadata.state;
+  document.querySelector('#stateInput').style.display = 'none';
+  zipDiv.innerHTML = user.user_metadata.zipcode;
+  document.querySelector('#zipInput').style.display = 'none';
+  document.querySelector('#profileBtn').style.display = 'none';
 };
 // open function opens the netlify-identity-widget signup/sign-in modal
 // const open = () => netlifyIdentity.open();
@@ -31,12 +45,6 @@ netlifyIdentity.on('login', (user) => {
   showCurrentInfo(user);
 });
 
-const redirectUserOnLogout = () => {
-  window.location.replace('login.html');
-  netlifyIdentity.close();
-};
-
-netlifyIdentity.on('logout', redirectUserOnLogout);
 // if (user.user_metadata.full_name) {
 //   textArea.value = format_stringify(
 //     user.user_metadata.full_name
@@ -62,10 +70,36 @@ netlifyIdentity.on('logout', redirectUserOnLogout);
 // adding some event listeners.
 // document.querySelector('#open').addEventListener('click', open);
 
-const updateStreet = (event) => {
+const updateProfile = async (event) => {
   event.preventDefault();
-  let inputVal = document.querySelector('#streetInput').value;
-  console.log(inputVal);
-  document.querySelector('#streetInput').value = inputVal;
+  let streetVal = document.querySelector('#streetInput').value;
+  document.querySelector('#streetDiv').innerHTML = streetVal;
+  document.querySelector('#streetInput').style.display = 'none';
+  let cityVal = document.querySelector('#cityInput').value;
+  document.querySelector('#cityDiv').innerHTML = cityVal;
+  document.querySelector('#cityInput').style.display = 'none';
+  let stateVal = document.querySelector('#stateInput').value;
+  document.querySelector('#stateDiv').innerHTML = stateVal;
+  document.querySelector('#stateInput').style.display = 'none';
+  let zipVal = document.querySelector('#zipInput').value;
+  document.querySelector('#zipDiv').innerHTML = zipVal;
+  document.querySelector('#zipInput').style.display = 'none';
+  const user_metadata = {
+    data: {
+      street_address: document.querySelector('#streetDiv').innerHTML,
+      city: document.querySelector('#cityDiv').innerHTML,
+      state: document.querySelector('#stateDiv').innerHTML,
+      zipcode: document.querySelector('#zipDiv').innerHTML,
+    },
+  };
+  const user = await netlifyIdentity.gotrue.currentUser().update(user_metadata);
+  console.log(user);
 };
-document.querySelector('#profileBtn').addEventListener('click', updateStreet);
+document.querySelector('#profileBtn').addEventListener('click', updateProfile);
+
+const redirectUserOnLogout = () => {
+  window.location.replace('login.html');
+  netlifyIdentity.close();
+};
+
+netlifyIdentity.on('logout', redirectUserOnLogout);
