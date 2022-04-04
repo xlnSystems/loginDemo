2c426784f2454f87fe4ae072e2e6728b2d89ef8e
xlnSystems <sasa@xlnsystems.com>
2022-04-04 11:18:58 -0400

    added sample calendar to booking page

--- profileEmail.js
+++ profileEmail.js
@@ -20,6 +20,10 @@ const updateEmail = () => {
       console.log('update email link sent');
       emailBox.style.display = 'none';
       document.querySelector('#emailMessage').style.display = 'block';
+      localStorage.setItem(
+        'emailDiv.innerHTML',
+        JSON.stringify(emailDiv.innerHTML)
+      );
     })
     .catch((error) => console.log('Failed to verify recover token: %o', error));
 };
@@ -30,3 +34,7 @@ emailInput.addEventListener('keydown', function (e) {
     updateEmail(e);
   }
 });
+
+// Retrieve the object from storage
+let newEmail = localStorage.getItem('emailDiv.innerHTML');
+emailDiv.innerHTML = JSON.parse(newEmail);
